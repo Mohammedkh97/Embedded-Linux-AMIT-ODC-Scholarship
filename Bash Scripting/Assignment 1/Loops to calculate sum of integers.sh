afe0ce59c590b0b694f4c500f6836e750487bb7e
Mohammed Khalaf <mohamedkhalaf20172020@gmail.com>
2025-01-31 05:22:00 +0200

    add another methods with the same behaviour

--- Bash Scripting/Assignment 1/Loops to calculate sum of integers.sh
+++ Bash Scripting/Assignment 1/Loops to calculate sum of integers.sh
@@ -1,7 +1,8 @@
 # Write a shell script that calculates the sum of integers from 1 to N using a loop
 !#/bin/bash
-echo "Enter the value of N"
+echo "Enter the value of N"  # Another way to print a message with input   # echo -n "Enter the value of N: "  read N
 read N
+# read -p "Enter the value of N: " N    # Another way to read input in the same line 
 sum=0
 for((i=1;i<=N;i++))
 do
